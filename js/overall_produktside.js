06481b2cd29f6d56cbe72e7541fab1163165a446
ddop711 <ddop711@gmail.com>
2025-12-17 02:44:32 +0100

    denzel-del1

--- js/overall_produktside.js
+++ js/overall_produktside.js
@@ -4,27 +4,3 @@ document.getElementById("burger").onclick = () => {
 };
 
 ///////////////////////////// BURGER MENU ///////////////////////////////
-
-//Test
-const sword = document.getElementById("hotspotSword-2");
-const overskrift = document.querySelector(".info-text > h2");
-const underrubrik = document.querySelector(".placeholder");
-const tekst1 = document.querySelector("#efficiency");
-const tekst2 = document.querySelector("#requirement");
-
-sword.addEventListener("mouseover", hoverSword);
-function hoverSword() {
-  sword.style.fill = "red";
-}
-
-sword.addEventListener("mouseout", outSword);
-function outSword() {
-  sword.style.fill = "white";
-}
-
-sword.addEventListener("click", clickSword);
-function clickSword() {
-  overskrift.textContent = "Sværd";
-  underrubrik.textContent = "For at skade Enderdragen skal vi bruge våben. Få fat i et sværd, gerne af netherite, men diamant er også fint.";
-  tekst1.innerHTML = `<img src="img/sword.png" alt="sword";">`;
-}
